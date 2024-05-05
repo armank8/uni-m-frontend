3e8c71663960d9dc1fe23fab2231e9f92d6c41ab
Arman-Kabir <armankabir8@gmail.com>
2024-05-05 13:52:47 +0600

    sidebar styles globals styles

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -42,8 +42,10 @@ const Sidebar = () => {
     const [collapsed, setCollapsed] = useState(false);
     return (
 
-        <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-            <div className="demo-logo-vertical" />
+        <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)} width={280} style={{ overflow: "auto", height: "100vh", position: "sticky", left: 0, top: 0, bottom: 0 }}>
+            <div style={{ color: "white", fontSize: "2rem", textAlign: "center", fontWeight: "bold", marginBottom: "1rem" }}>
+                PH University
+            </div>
             <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
         </Sider>
 
