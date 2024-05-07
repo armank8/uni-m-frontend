449d2c46c5a2b5216d8c7f083f3758e42df3ef04
Arman-Kabir <armankabir8@gmail.com>
2024-05-07 11:26:50 +0600

    sidebar items component

--- src/constants/sidebarItems.tsx
+++ src/constants/sidebarItems.tsx
@@ -1,6 +1,28 @@
+import type { MenuProps } from 'antd';
+import {
+    DesktopOutlined,
+    FileOutlined,
+    PieChartOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
 
 export const sidebarItems = (role: string) => {
-    return (
-        <div>sidebarItems</div>
-    )
+    const defaultSidebarItems: MenuProps["items"] = [
+        {
+            label: "Profile",
+            key: "profile",
+            icon: < UserOutlined />,
+            children: [{
+                label: "Account Profile",
+                key: "profile"
+            },
+            {
+                label: "Change Password",
+                key: 'profile'
+            }
+            ],
+        }
+    ]
+    if(role==='student') return defaultSidebarItems;
 }
