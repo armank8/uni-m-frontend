f3a8d8c58f16ea75b5dbb7bb910f650e95576ae8
Arman-Kabir <armankabir8@gmail.com>
2024-05-07 17:47:04 +0600

    sidebar item dynamic and pre-codes added

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -43,7 +43,7 @@ const items: MenuItem[] = [
 const Sidebar = () => {
     const [collapsed, setCollapsed] = useState(false);
 
-    const role= USER_ROLE.ADMIN;
+    const role= USER_ROLE.STUDENT;
     return (
 
         <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)} width={280} style={{ overflow: "auto", height: "100vh", position: "sticky", left: 0, top: 0, bottom: 0 }}>
