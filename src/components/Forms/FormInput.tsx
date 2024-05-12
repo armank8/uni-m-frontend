62c01480f6498886bed53938db9520a4034682ff
Arman-Kabir <armankabir8@gmail.com>
2024-05-12 10:14:22 +0600

    input field

---
+++ src/components/Forms/FormInput.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useFormContext, Controller } from "react-hook-form"
+import { Input } from "antd";
+
+interface IInput {
+    type: string;
+    name: string;
+    size: "large" | "small"
+    value: string | string[] | undefined;
+    id?: string;
+    placeholder?: string;
+    validation?: object;
+    label?: string
+}
+export default function FormInput({ type, name, size, value, id, placeholder, validation, label }: IInput) {
+    const { control } = useFormContext();
+    return (
+        <>
+            {label ? label : null}
+            <Controller
+                control={control}
+                name={name}
+                render={({ field }) => (
+                    <Input
+                        type={type}
+                        size={size}
+                        placeholder={placeholder}
+                        {...field}
+                        value={value ? value : field.value}
+                    />
+                )}
+            />
+        </>
+    )
+}
